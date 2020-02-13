eec83eae15bbb89aeac639258f8547eb76ae3a21
Aaron Appel <36603239+AaronAppel@users.noreply.github.com>
2020-02-12 20:00:55 -0500

    Fix for missing macro definition

--- Systems/Profiler/Profiler.h
+++ Systems/Profiler/Profiler.h
@@ -9,6 +9,7 @@
 // NOTE: Could use __FUNCSIG__ to show args for overloaded function, but it is more info than needed currently
 #define PROFILE_FUNC() PROFILE_SCOPE(__FUNCTION__)
 #else
+#define PROFILE_SCOPE(scopeName)
 #define PROFILE_FUNC(scopeName)
 #endif
 
